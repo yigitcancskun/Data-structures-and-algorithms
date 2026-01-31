d3bc784849a4865d2fa3089778fff3870da87424
yigitcancskun <yigitcancskun@gmail.com>
2026-02-01 00:00:36 +0300

    append fonksiyonu eklendi

--- Node_linkedlist_relationship.py
+++ Node_linkedlist_relationship.py
@@ -15,29 +15,24 @@ class LinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
-            return temp
+            
 
     def append(self, value):
         new_node = Node(value)
-        if self.length == 0:
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
-            self.length += 1
-            print("Appended:", value)
-            return value
+        self.length += 1
+        return True
 
 
 
 
-
-my_linked_list = LinkedList(5)
-print(my_linked_list.head.value)  # Output: 4
-my_linked_list.append(10)
-my_linked_list.append(10)
-
-my_linked_list.append(10)
-my_linked_list.append(10)
-my_linked_list.print_list()  # Output: 4 -> 10
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.print_list()
